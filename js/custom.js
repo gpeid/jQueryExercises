1fbadd63b41fa130e6d2a4d4388945634de12e75
George <ghdiep@gmail.com>
2013-03-04 18:30:48 -0500

    commit

--- js/custom.js
+++ js/custom.js
@@ -1,26 +1,20 @@
 //	jQuery.noConflict();
-
-
 $(document).ready(function(){
 
-$('.field .field-type-text .field-field-teaser').siblings().css({'display' : 'none'});
+	$('.field .field-type-text .field-field-teaser').siblings().css({'display' : 'none'});
 
-$('div#exercise-box-container div').hover(
-	function(){
-	    $(this).css({'background' : '#ff66cc'});
-	},
-	function(){
-	    $(this).css({'background' : '#FFA500'});
+	$('div#exercise-box-container div').hover(
+		function(){
+		    $(this).css({'background' : '#ff66cc'});
+		},
+		function(){
+		    $(this).css({'background' : '#FFA500'});
+		});
+	$('#exercise-box-container > div').click(function(){
+		var className = $(this).attr('class');
+		$('#practice-box-container > div').fadeOut(1000);
+		$('#practice-box-container > div.' + className).fadeToggle(1000);
 	});
-
-
-$('#exercise-box-container > div').click(function(){
-	var className = $(this).attr('class');
-	$('#practice-box-container > div').fadeOut(1000);
-	$('#practice-box-container > div.' + className).fadeToggle(1000);
-
-
-});
 
 
 /*
@@ -43,8 +37,51 @@ $('div#click1').click(function(){
 	});
 */
 
+$('a.show-hide').click(function(){
+    $('div.show-hide').toggle();
+});
+$('a.fade-inout').click(function(){
+    $('div.fade-inout').fadeToggle();
+});
+$('a.slide-updown').click(function(){
+    $('div.slide-updown').slideToggle();
+});
 
 
+
+
+// Show Hide
+	$('h3.show').click(function(){
+	    $('p.show-hide').show();
+	});
+	$('h3.hide').click(function(){
+	    $('p.show-hide').hide();
+	});
+	$('h3.toggle').click(function(){
+	    $('p.show-hide').toggle();
+	});
+
+// Fade In/Out
+	$('h3.fadein').click(function(){
+	    $('p.fade-inout').fadeIn();
+	});
+	$('h3.fadeout').click(function(){
+	    $('p.fade-inout').fadeOut();
+	});
+	$('h3.fadetoggle').click(function(){
+	    $('p.fade-inout').fadeToggle();
+	});
+
+// Slide Up/Down
+	$('h3.slideup').click(function(){
+	    $('p.slide-updown').slideUp();
+	});
+	$('h3.slidedown').click(function(){
+	    $('p.slide-updown').slideDown();
+	});
+	$('h3.slidetoggle').click(function(){
+	    $('p.slide-updown').slideToggle();
+	});
 
 
 
